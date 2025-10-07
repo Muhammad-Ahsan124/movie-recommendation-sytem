e3169e72c7ce8eb84b4c89a6214cf45b82d66ece
Muhammad Ahsan <afzalahsan70@gmail.com>
2025-10-07 17:05:40 +0300

    Update two-tower.js

--- week4/two-tower.js
+++ week4/two-tower.js
@@ -1,102 +1,47 @@
-// two-tower.js
 class TwoTowerModel {
-  constructor(numUsers, numItems, embDim, useBPR = false) {
+  constructor(numUsers, numItems, embDim) {
     this.numUsers = numUsers;
     this.numItems = numItems;
     this.embDim = embDim;
-    this.useBPR = useBPR;
+
+    // Two embedding tables
     this.userEmbedding = tf.variable(tf.randomNormal([numUsers, embDim], 0, 0.05));
     this.itemEmbedding = tf.variable(tf.randomNormal([numItems, embDim], 0, 0.05));
-    this.optimizer = tf.train.adam(0.001);
   }
 
-  userForward(userIdx) { return tf.gather(this.userEmbedding, userIdx); }
-  itemForward(itemIdx) { return tf.gather(this.itemEmbedding, itemIdx); }
+  userForward(uIdxTensor) {
+    return tf.gather(this.userEmbedding, uIdxTensor);
+  }
+
+  itemForward(iIdxTensor) {
+    return tf.gather(this.itemEmbedding, iIdxTensor);
+  }
 
   score(uEmb, iEmb) {
-    return tf.sum(tf.mul(uEmb, iEmb), -1); // dot product
+    return tf.matMul(uEmb, iEmb, false, true);
   }
 
-  trainStep(userIdx, posIdx) {
-    return this.optimizer.minimize(() => {
-      const uEmb = this.userForward(userIdx);
-      const iEmb = this.itemForward(posIdx);
-      const logits = tf.matMul(uEmb, this.itemEmbedding, false, true); // in-batch negatives
-      const labels = tf.range(0, userIdx.shape[0], 1, 'int32');
-      const loss = this.useBPR
-        ? this.bprLoss(uEmb, iEmb)
-        : tf.losses.softmaxCrossEntropy(tf.oneHot(labels, logits.shape[1]), logits);
+  async trainBatch(uIdxs, iIdxs, optimizer) {
+    const u = tf.tensor1d(uIdxs, 'int32');
+    const i = tf.tensor1d(iIdxs, 'int32');
+    const lossFn = () => {
+      const uEmb = this.userForward(u);
+      const iEmb = this.itemForward(i);
+      const logits = this.score(uEmb, iEmb);
+      const labels = tf.tensor1d([...Array(uIdxs.length).keys()], 'int32');
+      const loss = tf.losses.softmaxCrossEntropy(
+        tf.oneHot(labels, iIdxs.length), logits
+      ).mean();
       return loss;
-    }, true);
+    };
+    const { value, grads } = tf.variableGrads(lossFn);
+    optimizer.applyGradients(grads);
+    const lossVal = value.dataSync()[0];
+    tf.dispose([u, i, value, grads]);
+    return lossVal;
   }
 
-  bprLoss(uEmb, iPos) {
-    const batchSize = uEmb.shape[0];
-    const negIdx = tf.randomUniform([batchSize], 0, this.numItems, 'int32');
-    const iNeg = this.itemForward(negIdx);
-    const posScore = this.score(uEmb, iPos);
-    const negScore = this.score(uEmb, iNeg);
-    return tf.neg(tf.mean(tf.logSigmoid(tf.sub(posScore, negScore))));
-  }
-
-  getUserEmbedding(uIdx) { return this.userForward(uIdx); }
-
-  getScoresForAllItems(uEmb) {
-    return tf.matMul(uEmb, this.itemEmbedding, false, true);
-  }
-}
-
-// Deep MLP Model
-class DeepMLPModel {
-  constructor(numUsers, numItems, embDim, userFeatDim, itemFeatDim) {
-    this.numUsers = numUsers;
-    this.numItems = numItems;
-    this.embDim = embDim;
-
-    this.userEmbedding = tf.variable(tf.randomNormal([numUsers, embDim], 0, 0.05));
-    this.itemEmbedding = tf.variable(tf.randomNormal([numItems, embDim], 0, 0.05));
-
-    // Project auxiliary features to embDim first
-    this.userDense = tf.layers.dense({ units: embDim, activation: 'relu' });
-    this.itemDense = tf.layers.dense({ units: embDim, activation: 'relu' });
-
-    // MLP head
-    this.hidden = tf.layers.dense({ units: embDim, activation: 'relu' });
-    this.outDense = tf.layers.dense({ units: 1 });
-
-    this.optimizer = tf.train.adam(0.001);
-  }
-
-  forwardUser(userIdx, userFeat) {
-    const uEmb = tf.gather(this.userEmbedding, userIdx);
-    const uFeatProj = this.userDense.apply(userFeat);
-    return tf.add(uEmb, uFeatProj);
-  }
-
-  forwardItem(itemIdx, itemFeat) {
-    const iEmb = tf.gather(this.itemEmbedding, itemIdx);
-    const iFeatProj = this.itemDense.apply(itemFeat);
-    return tf.add(iEmb, iFeatProj);
-  }
-
-  trainStep(userIdx, posIdx, userFeat, itemFeat) {
-    return this.optimizer.minimize(() => {
-      const uVec = this.forwardUser(userIdx, userFeat);
-      const iVec = this.forwardItem(posIdx, itemFeat);
-      const score = this.hidden.apply(tf.concat([uVec, iVec], -1));
-      const logits = this.outDense.apply(score);
-      const labels = tf.onesLike(logits);
-      const loss = tf.losses.sigmoidCrossEntropy(labels, logits);
-      return loss;
-    }, true);
-  }
-
-  getScoresForAllItems(uEmb, itemFeatMat) {
-    const uExp = uEmb.expandDims(1);
-    const tiled = tf.tile(uExp, [1, this.numItems, 1]);
-    const itemFeatProj = this.itemDense.apply(itemFeatMat);
-    const concat = tf.concat([tiled, itemFeatProj.expandDims(0).squeeze()], -1);
-    const h = this.hidden.apply(concat);
-    return this.outDense.apply(h).squeeze();
+  getUserEmbedding(uTensor) {
+    return this.userForward(uTensor);
   }
 }
